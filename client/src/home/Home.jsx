1cfd660003ab7bb5643a5eeed746974d04a8b162
Aditi Singh <aditidhn06@gmail.com>
2024-10-26 20:05:48 +0530

    file update

---
+++ client/src/home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Banner from '../components/Banner';
+import Feature from '../components/Feature';
+import Footer from '../components/Footer';
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Banner />
+      <Feature />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
